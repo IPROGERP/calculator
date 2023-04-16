d85a329ae497076db3a563825388381956ec199e
Ivan Petrov <ivanizatorPetroff@gmail.com>
2023-04-16 23:17:41 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func adddiction(x, y int) int {
+	return x + y
+}
+func devide(x, y int) int {
+	return x / y
+}
+func subtraction(x, y int) int {
+	return x - y
+}
+func multiplication(x, y int) int {
+	return x * y
+}
+func main() {
+	constanta := true
+	var x, y int
+	var usertext string
+	for constanta {
+		fmt.Print("enter x:")
+		fmt.Scan(&x)
+		fmt.Print("enter y:")
+		fmt.Scan(&y)
+		fmt.Print("enter aperation(divide,addiction,substraction,multiplication,stop):")
+		fmt.Scan(&usertext)
+		- = constanta
+		if usertext == "stop" {
+			constanta := false
+		}
+		if usertext == "divide" {
+			fmt.Print(devide(x, y))
+		}
+		if usertext == "addiction" {
+			fmt.Print(adddiction(x, y))
+		}
+		if usertext == "subtraction" {
+			fmt.Print(subtraction(x, y))
+		}
+		if usertext == "multiplication" {
+			fmt.Print(multiplication(x, y))
+		}
+	}
+
+}
